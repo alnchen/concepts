287b7213c3ee63407bccb31839112b652e9a5c5e
Allen Chen <alchen1231@gmail.com>
2017-10-19 18:09:00 -0700

    testing

---
+++ prep/require/app.js
@@ -0,0 +1,10 @@
+// import { faveMovies } from './movies.js';
+//
+// const body = document.getElementById('movies');
+//
+// faveMovies.forEach( movie => {
+//   console.log(movie);
+//   var item = document.createElement('p');
+//   item.innerHTML = movie;
+//   body.appendChild(item);
+// });
---
+++ prep/require/movies.js
@@ -0,0 +1,7 @@
+export const faveMovies = [
+  'Rush Hour',
+  'Avengers',
+  'Tangled',
+  'Oceans 11',
+  'Going The Distance'
+];
